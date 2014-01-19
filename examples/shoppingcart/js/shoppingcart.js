c1ad35db5dd03771a5134ef2297ec589f0d354fa
Per Sandstrom <pg.sandstrom@gmail.com>
2014-01-19 13:25:07 +0100

    shoppingcart example mostly done

--- examples/shoppingcart/js/shoppingcart.js
+++ examples/shoppingcart/js/shoppingcart.js
@@ -2,12 +2,43 @@ var shoppingcartApp = angular.module('shoppingcartApp', []);
 
 shoppingcartApp.controller('shoppingcartController', function FirstCtrl($scope, $http) {
 
-    $http.get('http://localhost:8080/products').success(function (data) {
-        $scope.products = data;
-        console.log("data: " + JSON.stringify(data));
-    }).error(function (data, status, headers, config) {
-            console.log("data fel: " + status);
-        });
+    $scope.getProducts = function () {
+        $http.get('http://localhost:8080/products').success(function (data) {
+            $scope.products = data;
+        }).error(function (data, status, headers, config) {
+                console.log("error: " + status);
+            });
+    };
 
-    console.log("klar");
+    $scope.buy = function (id) {
+        $http.post('http://localhost:8080/cart/store/' + id).success(function (data) {
+            $scope.updateTotal();
+            $scope.updateCart();
+        });
+    };
+
+    $scope.remove = function (id) {
+        $http.post('http://localhost:8080/cart/remove/' + id).success(function (data) {
+            $scope.updateTotal();
+            $scope.updateCart();
+        });
+    };
+
+    $scope.updateTotal = function () {
+        $http.post('http://localhost:8080/cart/total').success(function (data) {
+            $scope.total = data.totalPrice;
+        });
+    };
+
+    $scope.updateCart = function () {
+        $http.post('http://localhost:8080/cart/summary').success(function (data) {
+            $scope.cart = data;
+        });
+    };
+
+    //init all data:
+    $scope.getProducts();
+    $scope.updateTotal();
+    $scope.updateCart();
+
 });
