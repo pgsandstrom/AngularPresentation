555c81bfe3dd640febf0042bbea90f7e615d9e63
Per Sandstrom <pg.sandstrom@gmail.com>
2014-01-25 16:39:49 +0100

    improved route example

--- examples/deeplinking/js/deepLinking.js
+++ examples/deeplinking/js/deepLinking.js
@@ -1,35 +1,18 @@
 var myApp = angular.module('deepLinkingApp', ['ngRoute']);
 
-//myApp.directive('deepLinkingCtrl', function () {
-//    return {
-//        restrict: "E",
-//        template: "<div>Vår template!</div>"
-//    };
-//});
-//
-//myApp.directive('myAttribute', function () {
-//    return {
-//        restrict: "A",
-//        link: function (scope, element, attrs) {
-//            element.bind("mouseenter", function () {
-//                scope.$apply(attrs.myAttribute);
-//            });
-//        }
-//    }
-//})
-
-myApp.config(function ($routeProvider) {
+myApp.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
-        .when('/tab1', {
+        .when('/examples/deeplinking/tab1', {
             controller: 'tab1Ctrl',
             templateUrl: 'tab1.html'
         })
-        .when('/tab2', {
+        .when('/examples/deeplinking/tab2', {
             controller: 'tab2Ctrl',
             templateUrl: 'tab2.html'
         })
         .otherwise({
-            redirectTo: '/'
+            redirectTo: '/examples/deeplinking/'
         });
 });
 
@@ -44,11 +27,11 @@ myApp.controller('tab2Ctrl', function ($scope) {
 myApp.controller('deepLinkingCtrl', function ($scope, $location) {
 
     $scope.selectTab1 = function () {
-        $location.path('/tab1');
+        $location.path('/examples/deeplinking/tab1');
     };
 
     $scope.selectTab2 = function () {
-        $location.path('/tab2');
+        $location.path('/examples/deeplinking/tab2');
     };
 
 });
